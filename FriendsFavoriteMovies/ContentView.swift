da2a330c1aeb8b4511d0cb7916a614666f950fa5
soraya-ire <rujiangkongshi@gmail.com>
2026-04-03 16:10:16 +0900

    Initial Commit

---
+++ FriendsFavoriteMovies/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FriendsFavoriteMovies
+//
+//  Created by soraya irie on 2026/04/03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FriendsFavoriteMovies/FriendsFavoriteMoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendsFavoriteMoviesApp.swift
+//  FriendsFavoriteMovies
+//
+//  Created by soraya irie on 2026/04/03.
+//
+
+import SwiftUI
+
+@main
+struct FriendsFavoriteMoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
