01a0e7d4ba81d07c889b599a3d2a7a466b3cd1e3
soraya-ire <rujiangkongshi@gmail.com>
2026-04-04 22:29:56 +0900

    Complete Section4 Step3 - navigate sample data

--- FriendsFavoriteMovies/FriendList.swift
+++ FriendsFavoriteMovies/FriendList.swift
@@ -10,6 +10,7 @@ import SwiftData
 
 struct FriendList: View {
     @Query(sort: \Friend.name) private var friends: [Friend]
+    @Environment(\.modelContext) private var context
 
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
