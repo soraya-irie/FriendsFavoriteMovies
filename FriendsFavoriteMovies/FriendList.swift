6a4d86cd85a5b2023e6450c692a474a46f2caafa
soraya-ire <rujiangkongshi@gmail.com>
2026-04-04 21:59:05 +0900

    Complete Section4 Step1 - navigate sample data

---
+++ FriendsFavoriteMovies/FriendList.swift
@@ -0,0 +1,18 @@
+//
+//  FriendList.swift
+//  FriendsFavoriteMovies
+//
+//  Created by soraya irie on 2026/04/04.
+//
+
+import SwiftUI
+
+struct FriendList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FriendList()
+}
