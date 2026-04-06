5486b09887d5c48476380af2e9e3f1ffd08c1374
soraya-ire <rujiangkongshi@gmail.com>
2026-04-06 20:08:36 +0900

    Complete Section7 Step6 - navigate sample data

--- FriendsFavoriteMovies/MovieList.swift
+++ FriendsFavoriteMovies/MovieList.swift
@@ -13,7 +13,11 @@ struct MovieList: View {
     @Environment(\.modelContext) private var context
 
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List {
+            ForEach(movies) { movie in
+                Text(movie.title)
+            }
+        }
     }
 }
 
