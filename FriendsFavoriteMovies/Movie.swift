5ab52e8d754d7ab5f7ad2848aaa42f753e8ecc67
soraya-ire <rujiangkongshi@gmail.com>
2026-04-06 19:25:08 +0900

    Complete Section7 Step2 - navigate sample data

--- FriendsFavoriteMovies/Movie.swift
+++ FriendsFavoriteMovies/Movie.swift
@@ -21,5 +21,15 @@ class Movie {
     static let sampleData = [
         Movie(title: "Amusing Space Traveler 3",
               releaseDate: Date(timeIntervalSinceReferenceDate: -402_000_000)),
+        Movie(title: "Difficult Cat",
+              releaseDate: Date(timeIntervalSinceReferenceDate: -20_000_000)),
+        Movie(title: "Electrifying Trek",
+              releaseDate: Date(timeIntervalSinceReferenceDate: 300_000_000)),
+        Movie(title: "Reckless Train Ride 2",
+              releaseDate: Date(timeIntervalSinceReferenceDate: 120_000_000)),
+        Movie(title: "The Last Venture",
+              releaseDate: Date(timeIntervalSinceReferenceDate: 550_000_000)),
+        Movie(title: "Glamorous Neighbor",
+              releaseDate: Date(timeIntervalSinceReferenceDate: -1_700_000_000)),
     ]
 }
