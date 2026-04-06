fe6892edea9247351effc119af0f63cbb765bf89
soraya-ire <rujiangkongshi@gmail.com>
2026-04-06 22:42:10 +0900

    Complete Section1 Step2 - create, update, and delete data

---
+++ FriendsFavoriteMovies/FriendDetail.swift
@@ -0,0 +1,20 @@
+//
+//  FriendDetail.swift
+//  FriendsFavoriteMovies
+//
+//  Created by soraya irie on 2026/04/06.
+//
+
+import SwiftUI
+
+struct FriendDetail: View {
+    @Bindable var friend: Friend
+
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FriendDetail()
+}
