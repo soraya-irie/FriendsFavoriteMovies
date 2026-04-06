198fabb9ca93830dcf9a0ce7a77d101fffe26687
soraya-ire <rujiangkongshi@gmail.com>
2026-04-06 19:33:20 +0900

    Complete Section7 Step3 - navigate sample data

--- FriendsFavoriteMovies/SampleData.swift
+++ FriendsFavoriteMovies/SampleData.swift
@@ -40,5 +40,9 @@ class SampleData {
         for friend in Friend.sampleData {
             context.insert(friend)
         }
+
+        for movie in Movie.sampleData {
+            context.insert(movie)
+        }
     }
 }
