094d4189d2b12622f01351a868fa9b4555e14f9a
soraya-ire <rujiangkongshi@gmail.com>
2026-04-06 19:45:27 +0900

    Complete Section7 Step4 - navigate sample data

---
+++ FriendsFavoriteMovies/MovieList.swift
@@ -0,0 +1,19 @@
+//
+//  MovieList.swift
+//  FriendsFavoriteMovies
+//
+//  Created by soraya irie on 2026/04/06.
+//
+
+import SwiftUI
+
+struct MovieList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MovieList()
+        .modelContainer(SampleData.shared.modelContainer)
+}
