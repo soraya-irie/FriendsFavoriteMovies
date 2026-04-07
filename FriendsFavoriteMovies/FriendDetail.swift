08f4efbfd7403d0256b657f578c49b7fb516b14c
soraya-ire <rujiangkongshi@gmail.com>
2026-04-07 21:25:49 +0900

    Complete Section6 Step2 - create, update, and delete data

--- FriendsFavoriteMovies/FriendDetail.swift
+++ FriendsFavoriteMovies/FriendDetail.swift
@@ -10,6 +10,8 @@ import SwiftUI
 struct FriendDetail: View {
     @Bindable var friend: Friend
 
+    @Environment(\.dismiss) private var dismiss
+
     var body: some View {
         Form {
             TextField("Name", text: $friend.name)
