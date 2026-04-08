48a99c3c019a7ab2487e2ebc73421a24895503ee
soraya-ire <rujiangkongshi@gmail.com>
2026-04-08 10:58:09 +0900

    Complete Section7 Step7 - create, update, and delete data

--- FriendsFavoriteMovies/FriendList.swift
+++ FriendsFavoriteMovies/FriendList.swift
@@ -37,6 +37,7 @@ struct FriendList: View {
                 NavigationStack {
                     FriendDetail(friend: friend, isNew: true)
                 }
+                .interactiveDismissDisabled()
             }
         } detail: {
             Text("Select a friend")
