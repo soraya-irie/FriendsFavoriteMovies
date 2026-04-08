69bcd61b33c90f716da9248c15c6c40ec5f1b288
soraya-ire <rujiangkongshi@gmail.com>
2026-04-08 15:10:28 +0900

    Complete Section5 Step1 - work with relationships

---
+++ FriendsFavoriteMovies/FilteredMovieList.swift
@@ -0,0 +1,19 @@
+//
+//  FilteredMovieList.swift
+//  FriendsFavoriteMovies
+//
+//  Created by soraya irie on 2026/04/08.
+//
+
+import SwiftUI
+
+struct FilteredMovieList: View {
+    var body: some View {
+        MovieList()
+    }
+}
+
+#Preview {
+    FilteredMovieList()
+        .modelContainer(SampleData.shared.modelContainer)
+}
